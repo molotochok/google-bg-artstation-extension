1770295120bf1bd8f7eab0fe6adfcdd82dbfbd69
Markiian Karpa <markian98@gmail.com>
2022-12-20 22:17:19 +0100

    Cache response to improve performance

--- background.js
+++ background.js
@@ -15,10 +15,24 @@ chrome.management.onDisabled.addListener(reloadPages);
 chrome.management.onUninstalled.addListener(reloadPages);
 
 chrome.runtime.onMessage.addListener((_arg, _sender, sendResponse) => {
-  fetch("https://www.artstation.com/random_project.json")
-    .then(response => response.json())
-    .then(data => sendResponse(data))
-    .catch(error => console.error(error));
+  const STORAGE_KEY = "gbae_request_data";
+  const URL = "https://www.artstation.com/random_project.json";
+
+  chrome.storage.local.get(STORAGE_KEY).then((value) => {
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => chrome.storage.local.set({ [STORAGE_KEY]: data }))
+      .catch(error => console.error(error));
+
+    if (!value || !value[STORAGE_KEY]) {
+      fetch(URL)
+        .then(response => response.json())
+        .then(data => sendResponse(data))
+        .catch(error => console.error(error));
+    } else {
+      sendResponse(value[STORAGE_KEY]);
+    }
+  });
 
   return true;
 });
